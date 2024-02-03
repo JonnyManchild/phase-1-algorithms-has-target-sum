d24680fe98e9aa95008487db9546335e89d06e53
Jonathan MacDuff <jonathan.macduff@outlook.com>
2024-02-02 17:02:17 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+  for (let i=0; i < array.length; i++) {
+    let number = array[i];
+    for (let j=i+1; j < array.length; j++) {
+      let newNumber = array[j];
+      if (number + newNumber === target) {
+        return true
+      };
+    };
+  } return false;
+};
 
 /* 
-  Write the Big O time complexity of your function here
+  O[n^2]
 */
 
 /* 
-  Add your pseudocode here
+  take the first number of the array
+    add it to second number of array
+    check if matches target
+    add it to third number of array
+    etc to end of array
+  take second number of array
+    add it to third number of array
+    check if it matches target
+    etc to end of array
+  etc to end of array
 */
 
 /*
-  Add written explanation of your solution here
+  I want to take each number of the array, and add it to each other number, checking each sum against the target.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([128, 47, 315, 48, 119, 74], 202));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([128, 47, 315, 48, 119, 74], 1000));
+
+  console.log("");
+
   // add your own custom tests in here
+
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
